584514b0d3f2625ee4bcb57322c2dafd4d1fa607
nopears <gregorzhilich@gmail.com>
2024-05-10 21:11:30 +0300

    update neovim and configured lazyvim

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,127 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          cond = function()
+            return not focused
+          end,
+        },
+        view = "notify_send",
+        opts = { stop = false },
+      })
+
+      opts.commands = {
+        all = {
+          -- options for the message history that you get with `:Noice`
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      }
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+      render = "wrapped-compact",
+    },
+  },
+
+  -- LazyGit integration with Telescope
+  {
+    "kdheepak/lazygit.nvim",
+    keys = {
+      {
+        ";c",
+        ":LazyGit<Return>",
+        silent = true,
+        noremap = true,
+      },
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup({
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+
+          local function opts(desc)
+            return {
+              desc = "nvim-tree: " .. desc,
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+              nowait = true,
+            }
+          end
+
+          -- default mappings
+          api.config.mappings.default_on_attach(bufnr)
+
+          -- custom mappings
+          vim.keymap.set("n", "t", api.node.open.tab, opts("Tab"))
+        end,
+        actions = {
+          open_file = {
+            quit_on_open = true,
+          },
+        },
+        sort = {
+          sorter = "case_sensitive",
+        },
+        view = {
+          width = 30,
+          relativenumber = true,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+          custom = {
+            "node_modules/.*",
+          },
+        },
+        log = {
+          enable = true,
+          truncate = true,
+          types = {
+            diagnostics = true,
+            git = true,
+            profile = true,
+            watcher = true,
+          },
+        },
+      })
+    end,
+  },
+}
