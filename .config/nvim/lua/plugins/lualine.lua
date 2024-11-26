4722196d6fa497ec257c53aa786a118986252de3
nopears <gregorzhilich@gmail.com>
2024-11-26 16:48:42 +0300

    gruvbox lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,15 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    opts = { sections = { lualine_z = {
-      function()
-        return " " .. os.date("%I:%M %p")
-      end,
-    } } },
+    opts = {
+      theme = "gruvbox",
+      sections = {
+        lualine_z = {
+          function()
+            return " " .. os.date("%I:%M %p")
+          end,
+        },
+      },
+    },
   },
 }
