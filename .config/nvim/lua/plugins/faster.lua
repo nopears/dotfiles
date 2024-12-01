b452f5f0e254f5faeaacc2d669e31177014b450d
nopears <gregorzhilich@gmail.com>
2024-12-01 16:12:05 +0300

    some updates

--- .config/nvim/lua/plugins/faster.lua
+++ .config/nvim/lua/plugins/faster.lua
@@ -1,3 +1,28 @@
 return {
   "pteroctopus/faster.nvim",
+  opts = {
+    behaviours = {
+      bigfile = {
+        on = true,
+        features_disabled = {
+          "illuminate",
+          "matchparen",
+          "lsp",
+          "treesitter",
+          "indent_blankline",
+          "vimopts",
+          "filetype",
+          "lualine",
+          "syntax",
+        },
+        filesize = 2,
+        pattern = "*",
+        extra_patterns = {},
+      },
+      fastmacro = {
+        on = true,
+        features_disabled = { "lualine", "syntax", "treesitter", "lsp", "filetype", "indent_blankline" },
+      },
+    },
+  },
 }
