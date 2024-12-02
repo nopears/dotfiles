7854fbc4771b3a97e7a5e3c105e9fd26740d4c8c
nopears <gregorzhilich@gmail.com>
2024-12-02 10:37:06 +0300

    snacks bigfile

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,7 +3,28 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    bigfile = { enabled = true },
+    bigfile = {
+      enabled = true,
+      setup = function(ctx)
+        vim.cmd([[NoMatchParen]])
+        vim.cmd([[IBLDisableScope]])
+        vim.cmd([[IBLDisable]])
+        vim.cmd([[require("lualine").hide()]])
+        Snacks.util.wo(0, {
+          foldmethod = "manual",
+          statuscolumn = "",
+          conceallevel = 0,
+          swapfile = false,
+          undolevels = -1,
+          undoreload = 0,
+          list = false,
+        })
+        vim.b.minianimate_disable = true
+        vim.schedule(function()
+          vim.bo[ctx.buf].syntax = ""
+        end)
+      end,
+    },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
